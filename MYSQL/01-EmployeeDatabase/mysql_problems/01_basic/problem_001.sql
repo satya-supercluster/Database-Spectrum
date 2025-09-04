f8711d82c66cf7d33dbe52326514a2b23d7169c7
Satyam Gupta <satya.supercluster@gmail.com>
2025-09-04 15:15:25 +0530

    Update active identifier in problem_001.sql

--- MYSQL/01-EmployeeDatabase/mysql_problems/01_basic/problem_001.sql
+++ MYSQL/01-EmployeeDatabase/mysql_problems/01_basic/problem_001.sql
@@ -1,4 +1,4 @@
--- Active: 1755189953322@@127.0.0.1@3306@employee
+-- Active: 1756978004432@@127.0.0.1@3306@employee
 -- Problem 1: Basic SELECT
 -- Write a query to select all columns from the employee table
 -- Expected: Display all employee records
